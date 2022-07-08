d06d50470134b707ea1b18c1bea07e1d6fab3cf0
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-08 22:32:17 +0700

    add Notification and change button tabs UI

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -1,11 +1,14 @@
 import { Web3Provider } from "@ethersproject/providers";
-import { Text, Grid, Tabs, Button } from "@mantine/core";
+import { Grid, Button, Stack } from "@mantine/core";
 import { useWeb3React } from "@web3-react/core";
-import { BigNumber, ethers } from "ethers";
-import { Dispatch, MouseEventHandler, SetStateAction, useMemo, useState } from "react";
+import { ethers } from "ethers";
+import { Dispatch, SetStateAction, useState } from "react";
 import { BANK_CONTRACT_ADDRESS, MAX_INT } from "../../constants/const";
+import { useNotification } from "../../context/NotificationContext";
 import { bankService } from "../../services/bankService";
 import { daiService } from "../../services/tokenService";
+import handleNotificationFromResponse from "../../utils/handleNotificationFromResponse";
+import LoadingButton from "../Button/LoadingButton";
 import DepositModal from "../Modal/DepositModal";
 import TransferModal from "../Modal/TransferModal";
 import WithdrawModal from "../Modal/WithdrawModal";
@@ -26,32 +29,40 @@ interface IButtonGroupsProps {
 const ButtonGroups = ({ setDepositModalOpen, setWithdrawModalOpen, setTransferModalOpen }: IButtonGroupsProps) => {
   return (
     <>
-      <Grid.Col span={4}>
-        <Button
-          onClick={() => {
-            setDepositModalOpen(true);
-          }}
+      <Grid.Col
+        span={12}
+        sx={{
+          width: "100%",
+        }}
+      >
+        <Stack
+          sx={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", rowGap: "1rem" }}
         >
-          Deposit
-        </Button>
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Button
-          onClick={() => {
-            setWithdrawModalOpen(true);
-          }}
-        >
-          Withdraw
-        </Button>
-      </Grid.Col>
-      <Grid.Col span={4}>
-        <Button
-          onClick={() => {
-            setTransferModalOpen(true);
-          }}
-        >
-          Transfer
-        </Button>
+          <Button
+            sx={{ width: "100%" }}
+            onClick={() => {
+              setDepositModalOpen(true);
+            }}
+          >
+            Deposit
+          </Button>
+          <Button
+            sx={{ width: "100%" }}
+            onClick={() => {
+              setWithdrawModalOpen(true);
+            }}
+          >
+            Withdraw
+          </Button>
+          <Button
+            sx={{ width: "100%" }}
+            onClick={() => {
+              setTransferModalOpen(true);
+            }}
+          >
+            Transfer
+          </Button>
+        </Stack>
       </Grid.Col>
     </>
   );
@@ -59,15 +70,20 @@ const ButtonGroups = ({ setDepositModalOpen, setWithdrawModalOpen, setTransferMo
 
 const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) => {
   const { library, account } = useWeb3React<Web3Provider>();
+  const { addNotification } = useNotification();
   const [isDepositModalOpen, setDepositModalOpen] = useState<boolean>(false);
   const [isTransferModalOpen, setTransferModalOpen] = useState<boolean>(false);
   const [isWithdrawModalOpen, setWithdrawModalOpen] = useState<boolean>(false);
 
-  const handleOnAllowanceClick = async () => {
+  const handleAllowanceClick = async () => {
     if (!library || !account) {
       return;
     }
-    await daiService.approve(library, account, BANK_CONTRACT_ADDRESS, MAX_INT);
+
+    const response = await daiService.approve(library, account, BANK_CONTRACT_ADDRESS, MAX_INT);
+    console.log(response);
+
+    handleNotificationFromResponse(response, addNotification, "Successfully Allowance DAI Token.");
   };
 
   const handleDepositButtonClick = async (tokenAmount: string) => {
@@ -76,9 +92,10 @@ const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) =>
     }
     const tokenInputBignumber = ethers.utils.parseEther(tokenAmount);
     const response = await bankService.depositMoney(library, account, name, tokenInputBignumber);
-    if (response.wait) {
-      await response.wait();
-    }
+    console.log("response =", response.error.data.mesage);
+
+    handleNotificationFromResponse(response, addNotification, `Successfully deposit ${tokenAmount} DAI to ${account}`);
+
     toggleChange();
     setDepositModalOpen(false);
     return response;
@@ -90,9 +107,14 @@ const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) =>
     }
     const tokenInputBignumber = ethers.utils.parseEther(tokenAmount);
     const response = await bankService.withdrawMoney(library, account, name, tokenInputBignumber);
-    if (response.wait) {
-      await response.wait();
-    }
+    console.log(response);
+
+    handleNotificationFromResponse(
+      response,
+      addNotification,
+      `Successfully withdraw ${tokenAmount} DAI from ${account}`
+    );
+
     toggleChange();
     setWithdrawModalOpen(false);
     return response;
@@ -104,9 +126,13 @@ const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) =>
     }
     const tokenInputBignumber = ethers.utils.parseEther(tokenAmount);
     const response = await bankService.transferMoney(library, account, name, transferTo, tokenInputBignumber);
-    if (response.wait) {
-      await response.wait();
-    }
+
+    handleNotificationFromResponse(
+      response,
+      addNotification,
+      `Successfully transfer ${tokenAmount} DAI from ${account} to ${transferTo}`
+    );
+
     toggleChange();
     setTransferModalOpen(false);
     return response;
@@ -121,7 +147,7 @@ const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) =>
         alignItems: "center",
         margin: "2.5rem",
         rowGap: "0.5rem",
-        width: "25%",
+        width: "50%",
       }}
     >
       <Grid.Col span={6}>Account name:</Grid.Col>
@@ -155,9 +181,7 @@ const Account = ({ name, balance, toggleChange, isAllowance }: IAccountProps) =>
           setTransferModalOpen={setTransferModalOpen}
         />
       ) : (
-        <Button fullWidth onClick={handleOnAllowanceClick}>
-          Allowance Dai Token
-        </Button>
+        <LoadingButton handleOnClick={handleAllowanceClick} text="Allowance Dai Token" />
       )}
 
       <TransferModal
